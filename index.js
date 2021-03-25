65e5deedea5baa7d4b30179307948d9bac9bdec1
Andrew Curd <andrewc2695@gmail.com>
2021-03-25 14:56:43 -0700

    created basic msg with socket

--- index.js
+++ index.js
@@ -1,34 +1,24 @@
-const http = require('http');
-const socket = require('socket.io');
-const express = require('express');
-
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-var app = express();
-const server = http.createServer(app)
-const io = socket(server)
-
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
 
-    // just assign a random number to every player that has connected
-    // the numbers have no significance so it
-    // doesn't matter if 2 people get the same number
-    var playerId = Math.floor((Math.random() * 100) + 1)
-    console.log(playerId + ' connected');
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
 
-    // if a user disconnects just print their playerID
-    socket.on('disconnect', function () {
-        console.log(playerId + ' disconnected');
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
     });
 });
