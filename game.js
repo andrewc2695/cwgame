53e97348f6aba8530de04d4bd38dd64f9e8412a2
Andrew Curd <andrewc2695@gmail.com>
2021-03-25 22:47:12 -0700

    started to create board

--- game.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener("DOMContentloaded", () => {
-    console.log("hi");
-})
--- keys.js
+++
@@ -1,5 +0,0 @@
-const keys = {
-    pubKey: "pub - c - 5c0a37cc- 4dae-41d0 - b454 - a1d1491744c7",
-    subKey: "sub-c-e59abba6-8c35-11eb-9de7-3a1dcc291cdf",
-    secKey: "sec-c-ZGY4YTc5NjUtMTgzMS00MWIxLThmNWItYjZiNDU2YTIyMTFm"
-}
